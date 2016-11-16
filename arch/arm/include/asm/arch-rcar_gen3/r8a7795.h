4ad533d328b7c3911d9cfb791a71f8d2e3fc57d2
Hiroyuki Yokoyama <hiroyuki.yokoyama.vx@renesas.com>
2016-11-16 17:13:50 +0900

    arm: rcar_gen3: r8a7795: Fix RT-DMA/R-NANDC initial setting
    Stop clocks are RT-DMA1 and R-NANDC.
    
    Signed-off-by: Hiroyuki Yokoyama <hiroyuki.yokoyama.vx@renesas.com>

--- arch/arm/include/asm/arch-rcar_gen3/r8a7795.h
+++ arch/arm/include/asm/arch-rcar_gen3/r8a7795.h
@@ -2,7 +2,7 @@
  * arch/arm/include/asm/arch-rcar_gen3/r8a7795.h
  *	This file defines registers and value for r8a7795.
  *
- * Copyright (C) 2015 Renesas Electronics Corporation
+ * Copyright (C) 2015-2016 Renesas Electronics Corporation
  *
  * SPDX-License-Identifier:	GPL-2.0+
  */
@@ -13,7 +13,7 @@
 #include "rcar-base.h"
 
 /* Module stop control/status register bits */
-#define MSTP0_BITS	0x00200000
+#define MSTP0_BITS	0x00210000	/* change */
 #define MSTP1_BITS	0xFFFFFFFF
 #define MSTP2_BITS	0x340E2FDC
 #define MSTP3_BITS	0xFFFFFFDF
@@ -21,7 +21,7 @@
 #define MSTP5_BITS	0xC3FFFFFF
 #define MSTP6_BITS	0xFFFFFFFF
 #define MSTP7_BITS	0xFFFFFFFF
-#define MSTP8_BITS	0x01F1FFF4
+#define MSTP8_BITS	0x01F1FFF5	/* change */
 #define MSTP9_BITS	0xFFFFFFFE
 #define MSTP10_BITS	0xFFFEFFE0
 #define MSTP11_BITS	0x000000B7
