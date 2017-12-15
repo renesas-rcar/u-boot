renesas-rcar/u-boot

commit 3d299f29a8a14491dcc79e5e82696130a85e8a83

common: cmd_load: Add load address check
This patch guards the program area at Salvator-X board.

Signed-off-by: Hiroyuki Yokoyama <hiroyuki.yokoyama.vx@renesas.com>

common/cmd_load.c
@@ -150,6 +150,15 @@ static ulong load_serial(long offset)
 		case SREC_DATA3:
 		case SREC_DATA4:
 		    store_addr = addr + offset;
+#ifdef CONFIG_TARGET_SALVATOR_X
+		    if ((store_addr >= CONFIG_SYS_TEXT_BASE) &&
+				(store_addr <= CONFIG_SYS_INIT_SP_ADDR)) {
+			printf("Program area is can't over write(0x%08lX)\n",
+			       store_addr);
+			return ~0;
+		    }
+#endif
+
 #ifndef CONFIG_SYS_NO_FLASH
 		    if (addr2info(store_addr)) {
 			int rc;

commit 2f480fb06c389067c5594125a26c3cfad01479f2

common: cmd_load: Fix load address check setting dependency
This patch fixes the problem that the code activation setting of
the load address check is on a board-by-board basis. The activation
is changed to the setting for each SoC series to be used.

Fixes: 3d299f29a8a1 ("common: cmd_load: Add load address check")
Signed-off-by: Hiroyuki Yokoyama <hiroyuki.yokoyama.vx@renesas.com>

common/cmd_load.c
@@ -150,7 +150,7 @@ static ulong load_serial(long offset)
 		case SREC_DATA3:
 		case SREC_DATA4:
 		    store_addr = addr + offset;
-#if defined(CONFIG_TARGET_SALVATOR_X) || defined(CONFIG_TARGET_ULCB)
+#if defined(CONFIG_RCAR_GEN3)
 		    if ((store_addr >= CONFIG_SYS_TEXT_BASE) &&
 				(store_addr <= CONFIG_SYS_INIT_SP_ADDR)) {
 			printf("Program area is can't over write(0x%08lX)\n",