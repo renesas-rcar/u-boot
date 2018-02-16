d234a54a1694fb40f4f693ab5bc3ec7c4527bf0c
Hiroyuki Yokoyama <hiroyuki.yokoyama.vx@renesas.com>
2018-02-16 14:12:34 +0900

    armv8: rcar_gen3: Add PFC/GPIO r8a77990 base code
    This patch is the base code of PFC/GPIO for R8A77990.
    
    Signed-off-by: Hiroyuki Yokoyama <hiroyuki.yokoyama.vx@renesas.com>

---
+++ arch/arm/include/asm/arch-rcar_gen3/r8a77990.h
@@ -0,0 +1,21 @@
+/*
+ * arch/arm/include/asm/arch-rcar_gen3/r8a77990.h
+ *	This file defines registers and value for r8a77990.
+ *
+ * Copyright (C) 2018 Renesas Electronics Corporation
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __ASM_ARCH_R8A77990_H
+#define __ASM_ARCH_R8A77990_H
+
+#include "rcar-base.h"
+
+/* SDHI */
+#define CONFIG_SYS_SH_SDHI0_BASE 0xEE100000
+#define CONFIG_SYS_SH_SDHI1_BASE 0xEE120000
+#define CONFIG_SYS_SH_SDHI3_BASE 0xEE160000
+#define CONFIG_SYS_SH_SDHI_NR_CHANNEL 3
+
+#endif /* __ASM_ARCH_R8A77990_H */
