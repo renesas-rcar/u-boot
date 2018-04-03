2ee0f32133e9c6fa3cab968536a47ae429d28506
Hiroyuki Yokoyama <hiroyuki.yokoyama.vx@renesas.com>
2018-04-03 18:06:28 +0900

    armv8: rcar_gen3: Add mstp_wait_clrbits macro function
    This patch adds a macro function for checking the status of clock
    supply start.
    
    Signed-off-by: Hiroyuki Yokoyama <hiroyuki.yokoyama.vx@renesas.com>

--- arch/arm/include/asm/arch-rcar_gen3/rcar-mstp.h
+++ arch/arm/include/asm/arch-rcar_gen3/rcar-mstp.h
@@ -2,7 +2,7 @@
  * arch/arm/include/asm/arch-rcar_gen3/rcar-mstp.h
  *	This file defines lowlevel functions for MSTP registers.
  *
- * Copyright (C) 2015-2017 Renesas Electronics Corporation
+ * Copyright (C) 2015-2018 Renesas Electronics Corporation
  *
  * SPDX-License-Identifier:	GPL-2.0+
  */
@@ -22,5 +22,27 @@
 		mstp_clrbits(le32, addr, saddr, clear)
 #define mstp_setclrbits_le32(addr, set, clear) \
 		mstp_setclrbits(le32, addr, set, clear)
+
+#define mstp_wait_clrbits(type, reg, clear)	\
+({						\
+	unsigned int timeout = 10000;		\
+	int ret = true;				\
+						\
+	while (in_##type(reg) & (clear)) {	\
+		timeout--;			\
+		udelay(10);			\
+		if (timeout == 0) {		\
+			/* false, but not yet init print */ \
+			ret = false;		\
+			break;			\
+		}				\
+	}					\
+	ret;					\
+});
+
+#define mstp_wait_clrbits_le32(reg, clear) \
+	mstp_wait_clrbits(le32, reg, clear)
+
+
 
 #endif /* __ASM_ARCH_RCAR_MSTP_H */
