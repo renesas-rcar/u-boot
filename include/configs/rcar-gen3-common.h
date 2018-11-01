a2dbd6a2da101b5c1cdc2dfe01c425e7e805943c
Hiroyuki Yokoyama <hiroyuki.yokoyama.vx@renesas.com>
2018-11-01 18:50:07 +0900

    ARM: renesas: Add usb_pgood_delay 2sec default where applicable on Gen3
    Some USB storage devices take longer time than usual to detect and
    power it up. Increase the delay where applicable on R-Car Gen3
    
    Signed-off-by: Hiroyuki Yokoyama <hiroyuki.yokoyama.vx@renesas.com>
    Signed-off-by: Hai Pham <hai.pham.ud@renesas.com>

--- include/configs/rcar-gen3-common.h
+++ include/configs/rcar-gen3-common.h
@@ -52,7 +52,14 @@
 
 /* ENV setting */
 
+#if CONFIG_IS_ENABLED(CMD_USB)
+#define EXTRA_ENV_RCAR_BOARD_SETTINGS "usb_pgood_delay=2000\0"
+#else
+#define EXTRA_ENV_RCAR_BOARD_SETTINGS "\0"
+#endif
+
 #define CONFIG_EXTRA_ENV_SETTINGS	\
+	EXTRA_ENV_RCAR_BOARD_SETTINGS	\
 	"bootm_size=0x10000000\0"
 
 #define CONFIG_BOOTCOMMAND	\
