205ba081a232f0e686d85ee90ef3ac83d618da0e
Hiroyuki Yokoyama <hiroyuki.yokoyama.vx@renesas.com>
2018-11-28 18:20:34 +0900

    board: salvator-x: Add function to activate with suitable DT
    This patch uses the IPL board identification function. It also
    acquires the mounted DRAM size information. Use these to select
    a suitable DT file for each board and start up.
    
    Signed-off-by: Hiroyuki Yokoyama <hiroyuki.yokoyama.vx@renesas.com>
    [haipham: rebase on u-boot/v2020.01]
    Signed-off-by: Hai Nguyen Pham <hai.pham.ud@renesas.com>

---
+++ board/renesas/rcar-common/board_detect.h
@@ -0,0 +1,26 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (C) 2019 Renesas Electronics Corporation
+ */
+
+#define BOARD_ID_SALVATOR_X	0x00
+#define BOARD_ID_SALVATOR_XS	0x04
+#define BOARD_ID_UNKNOWN	0xff
+#define BOARD_REV_UNKNOWN	0xff
+
+struct rcar_dram_conf_t {
+	u64 dram_type;
+	u64 legacy_address;
+	u64 legacy_size;
+	u64 address[4];
+	u64 size[4];
+};
+
+struct rcar_dt_fit_t {
+	unsigned char board_id;
+	unsigned char board_rev;
+	const char *target_name;
+};
+
+int32_t board_detect_type(struct rcar_dt_fit_t *dt_fit);
+int32_t board_detect_dram(struct rcar_dram_conf_t *dram_conf_addr);
