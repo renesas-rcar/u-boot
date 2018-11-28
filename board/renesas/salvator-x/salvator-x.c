37db17863ebc0acfddf615c7a069e0c3c8ba3c61
Hiroyuki Yokoyama <hiroyuki.yokoyama.vx@renesas.com>
2018-11-28 18:20:34 +0900

    board: salvator-x: Add function to activate with suitable DT
    This patch uses the IPL board identification function. It also
    acquires the mounted DRAM size information. Use these to select
    a suitable DT file for each board and start up.
    
    Signed-off-by: Hiroyuki Yokoyama <hiroyuki.yokoyama.vx@renesas.com>

--- board/renesas/salvator-x/salvator-x.c
+++ board/renesas/salvator-x/salvator-x.c
@@ -3,7 +3,7 @@
  * board/renesas/salvator-x/salvator-x.c
  *     This file is Salvator-X/Salvator-XS board support.
  *
- * Copyright (C) 2015-2017 Renesas Electronics Corporation
+ * Copyright (C) 2015-2019 Renesas Electronics Corporation
  * Copyright (C) 2015 Nobuhiro Iwamatsu <iwamatsu@nigauri.org>
  */
 
@@ -24,6 +24,7 @@
 #include <asm/arch/sh_sdhi.h>
 #include <i2c.h>
 #include <mmc.h>
+#include "../rcar-common/board_detect.h"
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -117,7 +118,7 @@ void board_add_ram_info(int use_default)
 		       (unsigned long long)(gd->bd->bi_dram[i].start
 		       + gd->bd->bi_dram[i].size - 1));
 		print_size(gd->bd->bi_dram[i].size, "\n");
-	};
+	}
 }
 
 void board_cleanup_before_linux(void)
@@ -136,3 +137,87 @@ void board_cleanup_before_linux(void)
 	mstp_setbits_le32(SMSTPCR9, SMSTPCR9,
 			  GPIO2_MSTP910 | GPIO3_MSTP909 | GPIO5_MSTP907);
 }
+
+#if defined(CONFIG_MULTI_DTB_FIT)
+int board_fit_config_name_match(const char *name)
+{
+	int ret;
+#if defined(CONFIG_R8A7795)
+	int i;
+	int bank_num;
+	u64 bank_size;
+#endif
+	struct rcar_dram_conf_t dram_conf_addr;
+	struct rcar_dt_fit_t dt_fit;
+
+	dt_fit.board_id = 0xff;
+	dt_fit.board_rev = 0xff;
+	/*
+	 * The name address register may be overwritten by the board_detect
+	 * function. Backup it.
+	 */
+	dt_fit.target_name = name;
+
+	ret = board_detect_type(&dt_fit);
+	if (ret)
+		return -1;
+
+	ret = board_detect_dram(&dram_conf_addr);
+
+	switch (dt_fit.board_id) {	 /* only supported board */
+	case BOARD_ID_SALVATOR_X:
+#if defined(CONFIG_R8A7795)
+		if (!strcmp(dt_fit.target_name, "r8a7795-salvator-x-u-boot"))
+			return 0;
+#endif
+#if defined(CONFIG_R8A7796)
+		if (!strcmp(dt_fit.target_name, "r8a7796-salvator-x-u-boot"))
+			return 0;
+#endif
+#if defined(CONFIG_R8A77965)
+		if (!strcmp(dt_fit.target_name, "r8a77965-salvator-x-u-boot"))
+			return 0;
+#endif
+		break;
+	case BOARD_ID_SALVATOR_XS:
+#if defined(CONFIG_R8A7795)
+		if (!ret) {
+			/* select memory type */
+			bank_num = 0;
+			for (i = 0; i < 4; i++) {
+				if (dram_conf_addr.address[i])
+					bank_num++;
+				else
+					break;
+			};
+			bank_size = dram_conf_addr.size[0];
+			if (!strcmp(dt_fit.target_name,
+				    "r8a7795-salvator-xs-u-boot") &&
+			    bank_num == 4 && bank_size == 0x40000000) {
+				return 0;
+			} else if (!strcmp(dt_fit.target_name,
+					   "r8a7795-salvator-xs-4x2g-u-boot") &&
+				   bank_num == 4 && bank_size == 0x80000000) {
+				return 0;
+			} else if (!strcmp(dt_fit.target_name,
+					   "r8a7795-salvator-xs-2x2g-u-boot") &&
+				   bank_num == 2 && bank_size == 0x80000000) {
+				return 0;
+			}
+		}
+		/* else works default : return -1 */
+#endif
+#if defined(CONFIG_R8A7796)
+		if (!strcmp(dt_fit.target_name, "r8a7796-salvator-xs-u-boot"))
+			return 0;
+#endif
+#if defined(CONFIG_R8A77965)
+		if (!strcmp(dt_fit.target_name, "r8a77965-salvator-xs-u-boot"))
+			return 0;
+#endif
+		break;
+	}
+
+	return -1;
+}
+#endif
