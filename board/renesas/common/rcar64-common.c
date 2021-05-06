373dcfc98dec353bc39242d292851a6ef7ac8754
Hai Pham <hai.pham.ud@renesas.com>
2021-05-06 10:09:41 +0700

    ARM: renesas: Print RAM configuration
    Print RAM configuration, which is passed to boot loader from prior-stage
    firmware. The function is based on show_dram_config() and "board:
    salvator-x: Add print RAM bank information" by Hiroyuki Yokoyama
    <hiroyuki.yokoyama.vx@renesas.com>
    
    Signed-off-by: Hai Pham <hai.pham.ud@renesas.com>
    Signed-off-by: Khanh Le <khanh.le.xr@renesas.com>

--- board/renesas/common/rcar64-common.c
+++ board/renesas/common/rcar64-common.c
@@ -7,6 +7,7 @@
  * Copyright (C) 2015 Nobuhiro Iwamatsu <iwamatsu@nigauri.org>
  */
 
+#include <display_options.h>
 #include <dm.h>
 #include <fdt_support.h>
 #include <hang.h>
@@ -59,6 +60,22 @@ int dram_init_banksize(void)
 	}
 
 	return 0;
+}
+
+void board_add_ram_info(int use_default)
+{
+	int i;
+
+	printf("\nRAM Configuration:\n");
+	for (i = 0; i < CONFIG_NR_DRAM_BANKS; i++) {
+		if (!gd->bd->bi_dram[i].size)
+			break;
+		printf("Bank #%d: 0x%09llx - 0x%09llx, ", i,
+		       (unsigned long long)(gd->bd->bi_dram[i].start),
+		       (unsigned long long)(gd->bd->bi_dram[i].start
+		       + gd->bd->bi_dram[i].size - 1));
+		print_size(gd->bd->bi_dram[i].size, "\n");
+	}
 }
 
 int __weak board_init(void)
