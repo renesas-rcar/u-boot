f4f98d35f9f75ca8cf3bb40233d061d234a3380e
Hai Pham <hai.pham.ud@renesas.com>
2021-06-18 18:53:20 +0700

    mmc: Introduce mmc_send_abort_tuning()
    Based on Linux commit [1] and [2]
    
    If a tuning command times out, the card could still be processing it,
    which will cause problems for recovery. The eMMC specification says that
    CMD12 can be used to stop CMD21, so add a function that does that.
    
    [1] e711f03091097 ("mmc: mmc: Introduce mmc_abort_tuning()")
    [2] 21adc2e45f4e ("mmc: Improve function name when aborting a tuning
    cmd")
    
    Signed-off-by: Hai Pham <hai.pham.ud@renesas.com>
    Reviewed-by: Takeshi Kihara <takeshi.kihara.df@renesas.com>

--- drivers/mmc/mmc.c
+++ drivers/mmc/mmc.c
@@ -395,6 +395,25 @@ int mmc_send_tuning(struct mmc *mmc, u32 opcode, int *cmd_error)
 }
 #endif
 
+int mmc_send_abort_tuning(struct mmc *mmc, u32 opcode)
+{
+	struct mmc_cmd cmd;
+
+	/*
+	 * eMMC specification specifies that CMD12 can be used to stop a tuning
+	 * command, but SD specification does not, so do nothing unless it is
+	 * eMMC.
+	 */
+	if (opcode != MMC_CMD_SEND_TUNING_BLOCK_HS200)
+		return 0;
+
+	cmd.cmdidx = MMC_CMD_STOP_TRANSMISSION;
+	cmd.cmdarg = 0;
+	cmd.resp_type = MMC_RSP_R1;
+
+	return mmc_send_cmd(mmc, &cmd, NULL);
+}
+
 static int mmc_read_blocks(struct mmc *mmc, void *dst, lbaint_t start,
 			   lbaint_t blkcnt)
 {
