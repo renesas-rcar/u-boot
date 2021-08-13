aaecc2dfcad60967bdfc1f3615993930ce061c05
Hai Pham <hai.pham.ud@renesas.com>
2021-08-13 17:43:03 +0700

    mtd: spi: renesas: Add R8A779A0 V3U compatible
    Add compatible for R8A779A0 V3U.
    
    Signed-off-by: Hai Pham <hai.pham.ud@renesas.com>

--- drivers/spi/renesas_rpc_spi.c
+++ drivers/spi/renesas_rpc_spi.c
@@ -455,6 +455,7 @@ static const struct udevice_id rpc_spi_ids[] = {
 	{ .compatible = "renesas,rpc-r8a77970" },
 	{ .compatible = "renesas,rpc-r8a77980" },
 	{ .compatible = "renesas,rpc-r8a77995" },
+	{ .compatible = "renesas,rpc-r8a779a0" },
 	{ .compatible = "renesas,rpc-r7s72100" },
 	{ }
 };
