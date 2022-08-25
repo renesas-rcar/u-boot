marex/u-boot-sh

clk: renesas: Tear clock controller down last before booting OS
Once all the other drivers got torn down in preparation for the OS
to start, tear down the clock controller last. The clock controller
must be torn down last as some of the clock which get turned off
might have still been needed during the teardown stage of the other
drivers.

Signed-off-by: Marek Vasut <marek.vasut+renesas@gmail.com>
Cc: Nobuhiro Iwamatsu <iwamatsu@nigauri.org>
Cc: Simon Glass <sjg@chromium.org>
Cc: Tom Rini <trini@konsulko.com>

drivers/clk/renesas/r8a7795-cpg-mssr.c
@@ -377,4 +377,5 @@ U_BOOT_DRIVER(clk_r8a7795) = {
 	.ops		= &gen3_clk_ops,
 	.probe		= gen3_clk_probe,
 	.remove		= gen3_clk_remove,
+	.flags		= DM_FLAG_OS_PREPARE | DM_FLAG_REMOVE_LATE,
 };

drivers/clk/renesas/r8a7796-cpg-mssr.c
@@ -355,4 +355,5 @@ U_BOOT_DRIVER(clk_r8a7796) = {
 	.ops		= &gen3_clk_ops,
 	.probe		= gen3_clk_probe,
 	.remove		= gen3_clk_remove,
+	.flags		= DM_FLAG_OS_PREPARE | DM_FLAG_REMOVE_LATE,
 };

drivers/clk/renesas/r8a77965-cpg-mssr.c
@@ -353,4 +353,5 @@ U_BOOT_DRIVER(clk_r8a77965) = {
 	.ops		= &gen3_clk_ops,
 	.probe		= gen3_clk_probe,
 	.remove		= gen3_clk_remove,
+	.flags		= DM_FLAG_OS_PREPARE | DM_FLAG_REMOVE_LATE,
 };

drivers/clk/renesas/r8a77970-cpg-mssr.c
@@ -230,4 +230,5 @@ U_BOOT_DRIVER(clk_r8a77970) = {
 	.ops		= &gen3_clk_ops,
 	.probe		= gen3_clk_probe,
 	.remove		= gen3_clk_remove,
+	.flags		= DM_FLAG_OS_PREPARE | DM_FLAG_REMOVE_LATE,
 };

drivers/clk/renesas/r8a77980-cpg-mssr.c
@@ -253,4 +253,5 @@ U_BOOT_DRIVER(clk_r8a77980) = {
 	.ops		= &gen3_clk_ops,
 	.probe		= gen3_clk_probe,
 	.remove		= gen3_clk_remove,
+	.flags		= DM_FLAG_OS_PREPARE | DM_FLAG_REMOVE_LATE,
 };

drivers/clk/renesas/r8a77990-cpg-mssr.c
@@ -312,4 +312,5 @@ U_BOOT_DRIVER(clk_r8a77990) = {
 	.ops		= &gen3_clk_ops,
 	.probe		= gen3_clk_probe,
 	.remove		= gen3_clk_remove,
+	.flags		= DM_FLAG_OS_PREPARE | DM_FLAG_REMOVE_LATE,
 };

renesas-rcar/u-boot

Signed-off-by: Tam Nguyen <tam.nguyen.xa@renesas.com>
Signed-off-by: Hai Pham <hai.pham.ud@renesas.com>

drivers/clk/renesas/r8a7795-cpg-mssr.c
@@ -390,4 +390,5 @@ U_BOOT_DRIVER(clk_r8a7795) = {
 	.ops		= &gen3_clk_ops,
 	.probe		= gen3_clk_probe,
 	.remove		= gen3_clk_remove,
+	.flags          = DM_FLAG_OS_PREPARE | DM_FLAG_VITAL,
 };

drivers/clk/renesas/r8a7796-cpg-mssr.c
@@ -393,4 +393,5 @@ U_BOOT_DRIVER(clk_r8a7796) = {
 	.ops		= &gen3_clk_ops,
 	.probe		= gen3_clk_probe,
 	.remove		= gen3_clk_remove,
+	.flags          = DM_FLAG_OS_PREPARE | DM_FLAG_VITAL,
 };

drivers/clk/renesas/r8a77965-cpg-mssr.c
@@ -372,4 +372,5 @@ U_BOOT_DRIVER(clk_r8a77965) = {
 	.ops		= &gen3_clk_ops,
 	.probe		= gen3_clk_probe,
 	.remove		= gen3_clk_remove,
+	.flags          = DM_FLAG_OS_PREPARE | DM_FLAG_VITAL,
 };

drivers/clk/renesas/r8a77970-cpg-mssr.c
@@ -235,4 +235,5 @@ U_BOOT_DRIVER(clk_r8a77970) = {
 	.ops		= &gen3_clk_ops,
 	.probe		= gen3_clk_probe,
 	.remove		= gen3_clk_remove,
+	.flags          = DM_FLAG_OS_PREPARE | DM_FLAG_VITAL,
 };

drivers/clk/renesas/r8a77980-cpg-mssr.c
@@ -255,4 +255,5 @@ U_BOOT_DRIVER(clk_r8a77980) = {
 	.ops		= &gen3_clk_ops,
 	.probe		= gen3_clk_probe,
 	.remove		= gen3_clk_remove,
+	.flags          = DM_FLAG_OS_PREPARE | DM_FLAG_VITAL,
 };

drivers/clk/renesas/r8a77990-cpg-mssr.c
@@ -330,4 +330,5 @@ U_BOOT_DRIVER(clk_r8a77990) = {
 	.ops		= &gen3_clk_ops,
 	.probe		= gen3_clk_probe,
 	.remove		= gen3_clk_remove,
+	.flags          = DM_FLAG_OS_PREPARE | DM_FLAG_VITAL,
 };