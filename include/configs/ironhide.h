8fb366cfe83ead41903d308a411de934a57eae0a
Hai Pham <hai.pham.ud@renesas.com>
2023-10-18 03:42:40 +0700

    arm64: renesas: Add R8A78000 Ironhide board code
    Add the initial support for Renesas R8A7800 board code
    
    Signed-off-by: Hai Pham <hai.pham.ud@renesas.com>

---
+++ include/configs/ironhide.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * include/configs/ironhide.h
+ *     This file is Ironhide board configuration.
+ *
+ * Copyright (C) 2025 Renesas Electronics Corp.
+ */
+
+#ifndef __IRONHIDE_H
+#define __IRONHIDE_H
+
+#include "rcar-gen5-common.h"
+
+#endif /* __IRONHIDE_H */
---
+++ include/configs/rcar-gen5-common.h
@@ -0,0 +1,27 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * include/configs/rcar-gen5-common.h
+ *	This file is R-Car Gen5 common configuration file.
+ *
+ * Copyright (C) 2025 Renesas Electronics Corporation
+ */
+
+#ifndef __RCAR_GEN5_COMMON_H
+#define __RCAR_GEN5_COMMON_H
+
+#include <asm/arch/renesas.h>
+
+/* console */
+#define CFG_SYS_BAUDRATE_TABLE		{ 38400, 115200, 921600, 1843200, 3250000 }
+
+/* Memory */
+#define DRAM_RSV_SIZE			0x08000000
+#define CFG_SYS_SDRAM_BASE		(0x40000000 + DRAM_RSV_SIZE)
+#define CFG_SYS_SDRAM_SIZE		(0x80000000u - DRAM_RSV_SIZE)
+#define CFG_MAX_MEM_MAPPED		(0x80000000u - DRAM_RSV_SIZE)
+
+/* Environment setting */
+#define CFG_EXTRA_ENV_SETTINGS		\
+	"bootm_size=0x10000000\0"
+
+#endif	/* __RCAR_GEN5_COMMON_H */
