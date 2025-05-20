6f888d4f1d8771dcabf06a3f3dfa276b8caf0bff
Khanh Le <khanh.le.xr@renesas.com>
2025-05-20 13:14:15 +0700

    arm64: renesas: Remove "ironhide" name from source code
    To be used in VDK environment.
    
    Signed-off-by: Khanh Le <khanh.le.xr@renesas.com>

---
+++ include/configs/rcar-x5h-board.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * include/configs/rcar-x5h-board.h
+ *     This file is R-Car X5H board configuration.
+ *
+ * Copyright (C) 2024 Renesas Electronics Corp.
+ */
+
+#ifndef __RCAR_X5H_BOARD_H
+#define __RCAR_X5H_BOARD_H
+
+#include "rcar-gen5-common.h"
+
+#endif /* __RCAR_X5H_BOARD_H */
