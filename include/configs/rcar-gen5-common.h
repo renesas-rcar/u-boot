eff132cec6c622289e170e4aac55f5d1a1086426
Hai Pham <hai.pham.ud@renesas.com>
2025-06-10 13:39:21 +0700

    arm64: renesas: gen5-common: Make fdt not relocate
    Set fdt_high to 0xFFFFFFFFFFFFFFFF as default so that fdt does not relocate
    
    Signed-off-by: Hai Pham <hai.pham.ud@renesas.com>

--- include/configs/rcar-gen5-common.h
+++ include/configs/rcar-gen5-common.h
@@ -22,6 +22,7 @@
 
 /* Environment setting */
 #define CFG_EXTRA_ENV_SETTINGS		\
-	"bootm_size=0x10000000\0"
+	"bootm_size=0x10000000\0"	\
+	"fdt_high=0xffffffffffffffff\0"
 
 #endif	/* __RCAR_GEN5_COMMON_H */
