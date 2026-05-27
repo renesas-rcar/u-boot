80c6f8a521a7741131d0dc166db15dc50fc78ae1
Khanh Le <khanh.le.xr@renesas.com>
2026-05-27 14:31:57 +0700

    arm64: dts: renesas: Add Renesas R-Car X5HAIH R8J78070 Perceptor board code
    Add initial support for Renesas R-Car X5HAIH R8J78070 Perceptor board.
    This consists mainly of DTs, Makefile and Kconfig entries and board
    specific configuration files.
    
    Signed-off-by: Khanh Le <khanh.le.xr@renesas.com>

---
+++ include/configs/perceptor.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (C) 2026 Renesas Electronics Corp.
+ */
+
+#ifndef __PERCEPTOR_H
+#define __PERCEPTOR_H
+
+#include "rcar-gen5-common.h"
+
+#endif /* __PERCEPTOR_H */
